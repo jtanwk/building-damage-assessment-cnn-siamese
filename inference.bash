8ec267e96ebab235bf11696b18c0707797b49bc3
Jonathan Tan <jon.twk@gmail.com>
2023-10-23 01:35:25 -0400

    Replace docker call with python3 call in inference.bash

--- inference.bash
+++ inference.bash
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 
-docker run \
-    --name "nlrc-model" \
-    --rm \
-    -v /datadrive/nlrc:/mnt \
-    nlrc-building-damage-assessment:latest \
-    "--output_dir" "/mnt" \
-    "--data_img_dir" "/mnt/dataset" \
-    "--data_inference_dict" "/mnt/constants/splits/all_disaster_splits_sliced_img_augmented_20.json" \
-    "--data_mean_stddev" "/mnt/constants/splits/all_disaster_mean_stddev_tiles_0_1.json" \
-    "--label_map_json" "/mnt/constants/class_lists/xBD_label_map.json" \
-    "--model" "/mnt/models/model_best.pth.tar"
+# docker run \
+#     --name "nlrc-model" \
+#     --rm \
+#     nlrc-building-damage-assessment:latest \
+#     "--output_dir" "outputs" \
+#     "--data_img_dir" "." \
+#     "--data_inference_dict" "constants/splits/test_jt.json" \
+#     "--data_mean_stddev" "constants/splits/all_disaster_mean_stddev_tiles_0_1.json" \
+#     "--label_map_json" "constants/class_lists/xBD_label_map.json" \
+#     "--model" "models/model_best.pth.tar"
+
+python3 inference/inference.py \
+    --output_dir outputs \
+    --data_img_dir ./raw/all_disaster_splits \
+    --data_inference_dict constants/splits/test_jt.json \
+    --data_mean_stddev constants/splits/all_disaster_mean_stddev_tiles_0_1.json \
+    --label_map_json constants/class_lists/xBD_label_map.json \
+    --model models/model_best.pth.tar
